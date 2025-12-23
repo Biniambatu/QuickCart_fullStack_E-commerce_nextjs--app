b3e32b233dca91ff185c8fc31373a6a4a1b767e1
Biniam batu <Biniambatu12@gmail.com>
2025-12-23 11:07:42 +0300

    commmit

--- config/inngest.ts
+++ config/inngest.ts
@@ -164,7 +164,7 @@ export const createUserOrder = inngest.createFunction(
                 items: event.data.items,
                 amount: event.data.amount,
                 address: event.data.address,
-                date: event.data.date
+                date: event.data.date || Date.now()
             }
         } )
 
